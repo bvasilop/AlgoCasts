d2c916f6208f514c734614a2df082784d4a58baf
bvasilop <bvasilop@gmail.com>
2019-12-13 10:08:05 -0800

    updated reverse string algorithm

--- completed_exercises/reversestring/index.js
+++ completed_exercises/reversestring/index.js
@@ -6,8 +6,18 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
+
 function reverse(str) {
-  return str.split('').reduce((rev, char) => char + rev, '');
+  let reversed = '';
+
+  for (const character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
 }
 
 module.exports = reverse;
@@ -17,14 +27,4 @@ module.exports = reverse;
 //     .split('')
 //     .reverse()
 //     .join('');
-// }
-
-// function reverse(str) {
-//   let reversed = '';
-//
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
-//
-//   return reversed;
 // }
