ac94e0dbde8629c77ae7e97d446587551b460922
bvasilop <bvasilop@gmail.com>
2019-12-17 20:46:29 -0800

    added reverseInt algorithm

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,20 +8,52 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//   str
+//     .toLowerCase()
+//     .split('')
+//     .filter(str => str > 'a' && str < 'z')
+//     .join('');
+//   return (
+//     str ===
+//     str
+//       .split('')
+//       .reverse()
+//       .join('')
+//   );
+// }
+// palindrome(`Madam I'm Adam`);
+
+// function palindrome(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '') === str;
+// }
+// palindrome(`Madam I'm Adam`);
+
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+//   return str === reversed;
+// }
+// palindrome(`Madam I'm Adam`);
+
 function palindrome(str) {
-  str
-    .toLowerCase()
+  return str
     .split('')
-    .filter(str => str > 'a' && str < 'z')
-    .join('');
-  return (
-    str ===
-    str
-      .split('')
-      .reverse()
-      .join('')
-  );
+    .every((char, index) => char === str[str.length - index - 1]);
+
+  // the first time we run our function we are at index 0.
+  // We could look at the entire string array and access the element at the length of the array minus one -- meaning the end of the array.
+  //  we increment for every step through the loop or through every function
+  // minus one and the minus one right here is to make sure that we are taking into account the fact that length is the overall length.
+  // We are zero indexed with javascript arrays.
 }
 palindrome(`Madam I'm Adam`);
+
+// function palindrome(str) {
+//   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+// }
+// palindrome(`Madam I'm Adam`);
 
 module.exports = palindrome;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,39 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/*
+// const myNumber = 200;
+// myNumber.toString().split(''); // [ '2', '0', '0' ]
+// parseInt(myNumber.toString()); // 200
+
+// Math.sign() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+
+// Math.sign(4000) // 1
+// Math.sign(-4000) // -1
+*/
+
+// function reverseInt(num) {
+//   const reversed = parseInt(
+//     num
+//       .toString()
+//       .split('')
+//       .reverse()
+//       .join('')
+//   );
+
+//   return reversed * Math.sign(num);
+// }
+// reverseInt(123);
+
+function reverseInt(n) {
+  const num = Math.abs(n);
+  const reversed = num
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+  return reversed * Math.sign(n);
+}
+reverseInt(123);
 
 module.exports = reverseInt;
